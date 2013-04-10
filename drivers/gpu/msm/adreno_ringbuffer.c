e6bd567364dc244b94ce8980f3438fb5a26516ad
Lucille Sylvester <lsylvest@codeaurora.org>
2013-04-10 14:30:29 -0600

    msm: kgsl: Use increasing delays to transition to NAP
    When active count goes down to zero, the GPU is about
    to be idle.  Wait for it to go fully idle and transition
    to the NAP state.
    
    Change-Id: I06ed7bc07125e757ae2d665b2fd4135a6e0296fe
    CRs-Fixed: 436977
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1081,6 +1081,7 @@ adreno_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 	}
 
 done:
+	device->pwrctrl.irq_last = 0;
 	kgsl_trace_issueibcmds(device, context ? context->id : 0, ibdesc,
 		numibs, *timestamp, flags, ret,
 		drawctxt ? drawctxt->type : 0);
